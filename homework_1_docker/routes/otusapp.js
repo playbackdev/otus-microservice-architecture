d0778b4ac2f80eeca03e1982d577d2b94ff6afa9
German Shvetsov <g.shvetsov@ekf.su>
2023-02-09 23:31:21 +0300

    fix

--- homework_1_docker/routes/otusapp.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const OtusAppController = require('../controllers/OtusAppController.js')
-
-router.get('/:student?', OtusAppController.hello)
-router.get('/:student/:param', OtusAppController.param)
-
-module.exports = router
